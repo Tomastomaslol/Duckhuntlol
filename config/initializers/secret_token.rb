2055edf1bfc7a3bd9d41853c929f40bacd33b19a
Tomas <tnygren@mediaingenunity.com>
2012-08-30 09:15:43 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duckhuntlol::Application.config.secret_token = '8acb004d63ddca6d06622e7e95da7228d3a350bf20f09286024ee892afe918703c9674c23c42fdc221e0f6d217876fcdf1cea58704fb4891a888a195fd2842a1'
